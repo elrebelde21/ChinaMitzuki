efd7d86984c30bc40b477b3a982e546e83473001
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-01-11 18:59:54 -0300

    Add files via upload

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -140,11 +140,11 @@ if (qr && mcode) {
 let secret = await sock.requestPairingCode((m.sender.split`@`[0]))
 secret = secret.match(/.{1,4}/g)?.join("-")
 const dispositivo = await getDevice(m.key.id)
-if (!m.isWABusiness && /web|desktop|unknown/i.test(dispositivo)) {
+if (!m.isWABusiness) {
 txtCode = await conn.sendMessage(m.chat, { image: { url: 'https://qu.ax/wyUjT.jpg' || imageUrl.getRandom() }, caption: rtx2.trim() + '\n' + drmer.toString("utf-8") }, { quoted: m })
 codeBot = await m.reply(secret)
 } else {
-txtCode = await conn.sendButton(m.chat, rtx2.trim() + '\n' + drmer.toString("utf-8"), `\n*Código:* ${secret}\n` + wm, 'https://qu.ax/wyUjT.jpg' || imageUrl.getRandom(), null,  [[`Copiar código`, secret]], null, null, m)
+txtCode = await conn.sendButton(m.chat, rtx2.trim() + '\n' + drmer.toString("utf-8"), `\n*Código:* ${secret}\n` + wm, 'https://qu.ax/wyUjT.jpg' || imageUrl.getRandom(), null, [[`Copiar código`, secret]], null, null, m)
 }
 console.log(secret)
 }

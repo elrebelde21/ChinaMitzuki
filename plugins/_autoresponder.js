64806b54217781acb77a94f2b495e553fc2abac2
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-01-12 19:46:54 -0300

    Update _autoresponder.js

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -8,13 +8,12 @@ let handler = m => m;
 let lastMessageTime = {};
 
 handler.all = async function (m, {conn}) {
+let user = global.db.data.users[m.sender]   
+let chat = global.db.data.chats[m.chat]
 m.isBot = m.id.startsWith('BAE5') && m.id.length === 16 || m.id.startsWith('3EB0') && m.id.length === 12 || m.id.startsWith('3EB0') && (m.id.length === 20 || m.id.length === 22) || m.id.startsWith('B24E') && m.id.length === 20;
 if (m.isBot) return
-if (m.id.startsWith('NJX-')) return;
+if (m.id.startsWith('NJX-')) return;    
 if (m.fromMe || conn.user.jid === m.sender) return
-
-let user = global.db.data.users[m.sender]   
-let chat = global.db.data.chats[m.chat];
 let prefixRegex = new RegExp('^[' + (opts['prefix'] || '‎z/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.,\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
 
 //if (prefixRegex.test(m.text)) return true;

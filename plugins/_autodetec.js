7c6b7ddf9182f6734b92003829821b41fe6e2b3c
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-01-13 12:27:08 -0300

    Add files via upload

--- plugins/_autodetec.js
+++ plugins/_autodetec.js
@@ -4,7 +4,7 @@ import path from 'path';
 export async function before(m, { conn, participants, groupMetadata}) {
 if (!m.messageStubType || !m.isGroup) return
 let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://telegra.ph/file/2a1d71ab744b55b28f1ae.jpg')
-let img = await (await fetch(`${pp}`)).buffer()
+  let img = await (await fetch(`${pp}`)).buffer()
 let usuario = `@${m.sender.split`@`[0]}` 
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let chat = global.db.data.chats[m.chat]
@@ -35,17 +35,15 @@ await this.sendMessage(m.chat, { text: `${usuario} 𝙉𝙐𝙀𝙑𝘼 𝘿𝙀
 await this.sendMessage(m.chat, { text: `🔒 𝘼𝙃𝙊𝙍𝘼 *${m.messageStubParameters[0] == 'on' ? '𝙎𝙊𝙇𝙊 𝘼𝘿𝙈𝙄𝙉𝙎' : '𝙏𝙊𝘿𝙊𝙎'}* 𝙋𝙐𝙀𝘿𝙀 𝙀𝘿𝙄𝙏𝘼𝙍 𝙇𝘼 𝙄𝙉𝙁𝙊𝙍𝙈𝘼𝘾𝙄𝙊𝙉 𝘿𝙀𝙇 𝙂𝙍𝙐𝙋𝙊`, mentions: [m.sender] }, { quoted: fkontak, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
 } else if (chat.detect && m.messageStubType == 26) {
 await this.sendMessage(m.chat, { text: `𝙀𝙇 𝙂𝙍𝙐𝙋𝙊 *${m.messageStubParameters[0] == 'on' ? '𝙀𝙎𝙏𝘼 𝘾𝙀𝙍𝙍𝘼𝘿𝙊 🔒' : '𝙀𝙎𝙏𝘼 𝘼𝘽𝙄𝙀𝙍𝙏𝙊 🔓'}*\n ${m.messageStubParameters[0] == 'on' ? '𝙎𝙊𝙇𝙊 𝙇𝙊𝙎 𝘼𝘿𝙈𝙄𝙉𝙎 𝙋𝙐𝙀𝘿𝙀𝙉 𝙀𝙎𝘾𝙍𝙄𝘽𝙄𝙍' : '𝙔𝘼 𝙋𝙐𝙀𝘿𝙀𝙉 𝙀𝙎𝘾𝙍𝙄𝘽𝙄𝙍 𝙏𝙊𝘿𝙊𝙎'} 𝙀𝙉 𝙀𝙎𝙏𝙀 𝙂𝙍𝙐𝙋𝙊`, mentions: [m.sender] }, { quoted: fkontak, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
-} else if (chat.welcome && m.messageStubType == 27) {
-//if (global.conn.user.jid.split`@`[0] != conn.user.jid.split`@`[0]) return
-//let user = m.messageStubParameters[0].split`@`[0]
+} else if (chat.welcome && m.messageStubType == 27 && this.user.jid != global.conn.user.jid) {
 let subject = groupMetadata.subject
-let desc = groupMetadata.desc || ".";
-let textWel = `•──〘 *\`WELCOME\`* 〙──•\n\n✨ *Bienvenid@s @${m.messageStubParameters[0].split`@`[0]} a ${subject} ✨\n\n*En este grupo podras encontrar:*\n➤ *Amistades* 👥\n➤ *Desmadre* 💃🕺\n➤ *Una botsita sexy 😘*\n➤ *Puede solicitar mi lista de comando con:* #menu\n\n> *Aqui tiene la descripción del grupo, léela!! 🙌*\n${desc}\n\n> *🔰 𝗗𝗶𝘀𝗳𝗿𝘂𝘁𝗮 𝗱𝗲 𝘁𝘂 𝗘𝘀𝘁𝗮𝗱𝗶́𝗮 𝗲𝗻 𝗲𝗹 𝗚𝗿𝘂𝗽𝗼 🔰*`
+let descs = groupMetadata.desc || ".";
+let textWel = `•──〘 *\`WELCOME\`* 〙──•\n\n✨ *Bienvenid@s @${m.messageStubParameters[0].split`@`[0]} a ${subject} ✨\n\n*En este grupo podras encontrar:*\n➤ *Amistades* 👥\n➤ *Desmadre* 💃🕺\n➤ *Una botsita sexy 😘*\n➤ *Puede solicitar mi lista de comando con:* #menu\n\n> *Aqui tiene la descripción del grupo, léela!! 🙌*\n${descs}\n\n> *🔰 𝗗𝗶𝘀𝗳𝗿𝘂𝘁𝗮 𝗱𝗲 𝘁𝘂 𝗘𝘀𝘁𝗮𝗱𝗶́𝗮 𝗲𝗻 𝗲𝗹 𝗚𝗿𝘂𝗽𝗼 🔰*`
 await this.sendMessage(m.chat, { text: textWel, 
 contextInfo:{
 forwardingScore: 9999999,
 isForwarded: true, 
-mentions:[m.sender, m.messageStubParameters[0]],
+mentionedJid:[m.sender, m.messageStubParameters[0]],
 externalAdReply: {
 showAdAttribution: true,
 renderLargerThumbnail: true,
@@ -54,14 +52,14 @@ title: [wm, ' ' + wm + '😊', '🌟'].getRandom(),
 containsAutoReply: true,
 mediaType: 1, 
 sourceUrl: [nna, nna2, nnaa, yt, md].getRandom()}}}, { quoted: fkontak }) 
-} else if (chat.welcome && m.messageStubType == 32) {
-if (global.conn.user.jid.split`@`[0] != conn.user.jid.split`@`[0]) return
-let textBye = `Se fue @${m.messageStubParameters[0].split`@`[0]} un gays menos`
+} else if (chat.welcome && m.messageStubType == 32 && this.user.jid != global.conn.user.jid) {
+//if (global.conn.user.jid.split`@`[0] != conn.user.jid.split`@`[0]) return
+let textBye = `Bueno, se fue @${m.messageStubParameters[0].split`@`[0]} 👋\n\nQue dios lo bendiga 😎`
 await this.sendMessage(m.chat, { text: textBye, 
 contextInfo:{
 forwardingScore: 9999999,
 isForwarded: true, 
-mentions:[m.sender, m.messageStubParameters[0]],
+mentionedJid:[m.sender, m.messageStubParameters[0]],
 externalAdReply: {
 showAdAttribution: true,
 renderLargerThumbnail: true,
@@ -110,3 +108,4 @@ console.log({messageStubType: m.messageStubType,
 messageStubParameters: m.messageStubParameters,
 type: WAMessageStubType[m.messageStubType], 
 })}}
+
